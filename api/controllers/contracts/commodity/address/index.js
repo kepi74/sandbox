2ef0d1e6a58652641397db80f6abe7148717cab5
Pavel Kepka <pkepka@blueberryapps.com>
2017-04-17 09:56:26 +0200

    Prepare responses for submit address

--- api/controllers/contracts/commodity/address/index.js
+++ api/controllers/contracts/commodity/address/index.js
@@ -1,17 +1,99 @@
-var CODES_ERR_EDIT_IN_PROGRESS = '0001';
-
 var examples = {
   errEditInProgress: {
-    errors: [
-      {
-        code: "ERR_EDIT_IN_PROGRESS",
-        error_message: "Cannot request another change request. Request already queued today",
-        parameters: [],
-        placeholders: []
-      }
-    ]
-  }
+    statusCode: 400,
+    response: {
+      errors: [
+        {
+          code: 'ERR_EDIT_IN_PROGRESS',
+          'error_message': 'Cannot request another change request. Request already queued today',
+          parameters: [],
+          placeholders: []
+        }
+      ]
+    },
+  },
+  errorFieldPattern: {
+    statusCode: 400,
+    response: {
+      errors: [
+        {
+          code: 'ERR_FIELD_PATTERN',
+          'error_message': '',
+          parameters: 'zip_code',
+          placeholders: null
+        }
+      ]
+    },
+  },
+  errorFieldRequired: {
+    statusCode: 400,
+    response: {
+      errors: [
+        {
+          code: 'ERR_FIELD_REQUIRED',
+          'error_message': 'Missing mandatory param',
+          parameters: 'zip_code',
+          placeholders: null
+        }
+      ]
+    },
+  },
+  // TODO: Report invalid funkcionality of endpoint ERR_FIELD_LENGTH
+  errorFieldLength: {
+    response: {
+      errors: [
+        {
+          code: 'ERR_FIELD_LENGTH',
+          'error_message': '',
+          parameters: '',
+          placeholders: null
+        }
+      ]
+    },
+  },
+  // TODO: Report invalid funkcionality of endpoint ERR_NOT_FOUND_PSC
+  errorNotFoundPSC: {
+    response: {
+      errors: [
+        {
+          code: 'ERR_NOT_FOUND_PSC',
+          'error_message': 'Missing mandatory param',
+          parameters: 'zip_code',
+          placeholders: null
+        }
+      ]
+    },
+  },
+  errorUserInput: {
+    statusCode: 400,
+    response: {
+      errors: [
+        {
+          code: 'ERR_USER_INPUT',
+          'error_message': 'Invalid ZIP code',
+          parameters: 'zip_code',
+          placeholders: []
+        }
+      ]
+    }
+  },
+  success: {
+    statusCode: 202,
+    response: {},
+  },
 };
+
+zipCodes = {};
+zipCodes['00000'] = '500';
+zipCodes['00001'] = '404';
+zipCodes['00003'] = '403';
+zipCodes['10000'] = response.errorNotFoundPSC;
+zipCodes['10001'] = response.errorUserInput;
+zipCodes['10002'] = response.errorNotFoundPSC;
+zipCodes['10003'] = response.errorFieldRequired;
+zipCodes['10004'] = response.errorFieldPattern;
+
+houseNumberCodes = {};
 
 function(args, res, next) {
   /**
